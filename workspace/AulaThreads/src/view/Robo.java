630fd3158e091cf1d39f0b09d2dba2eb2fbc7fb0
RaphaelVictor203 <RaphaelVictor203@github.com>
2018-08-31 20:33:38 -0300

    Alterações na classe Robo

--- workspace/AulaThreads/src/view/Robo.java
+++ workspace/AulaThreads/src/view/Robo.java
@@ -20,13 +20,25 @@ public class Robo {
 		try {
 			Robot robozin = new Robot();
 			
-			robozin.mouseMove(60, d.height - 10);
+			//robozin.mouseMove(60, d.height - 10);
 			
 			robozin.delay(100);
 			robozin.setAutoDelay(100);
 			
-			robozin.mousePress(InputEvent.BUTTON1_MASK);
-			robozin.mouseRelease(InputEvent.BUTTON1_MASK);
+			//robozin.mousePress(InputEvent.BUTTON1_MASK);
+			//robozin.mouseRelease(InputEvent.BUTTON1_MASK);
+			
+			try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			robozin.keyPress(KeyEvent.VK_WINDOWS);
+			robozin.keyPress(KeyEvent.VK_R);
+			robozin.keyRelease(KeyEvent.VK_R);
+			robozin.keyRelease(KeyEvent.VK_WINDOWS);
 			
 			//ESCREVE CMD
 			robozin.keyPress(KeyEvent.VK_C);
@@ -134,7 +146,7 @@ public class Robo {
 			robozin.keyRelease(KeyEvent.VK_ENTER);
 			
 			try {
-				Thread.sleep(2000);
+				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -142,23 +154,51 @@ public class Robo {
 			
 			
 			//ESCREVE JAVA
-			robozin.keyPress(KeyEvent.VK_J);
-			robozin.keyRelease(KeyEvent.VK_J);
+			robozin.keyPress(KeyEvent.VK_R);
+			robozin.keyRelease(KeyEvent.VK_R);
 			
 			robozin.keyPress(KeyEvent.VK_A);
 			robozin.keyRelease(KeyEvent.VK_A);
+			
+			robozin.keyPress(KeyEvent.VK_P);
+			robozin.keyRelease(KeyEvent.VK_P);
+			
+			robozin.keyPress(KeyEvent.VK_H);
+			robozin.keyRelease(KeyEvent.VK_H);
+			
+			robozin.keyPress(KeyEvent.VK_A);
+			robozin.keyRelease(KeyEvent.VK_A);
+			
+			robozin.keyPress(KeyEvent.VK_E);
+			robozin.keyRelease(KeyEvent.VK_E);
+			
+			robozin.keyPress(KeyEvent.VK_L);
+			robozin.keyRelease(KeyEvent.VK_L);
+			
+			robozin.keyPress(KeyEvent.VK_SPACE);
+			robozin.keyRelease(KeyEvent.VK_SPACE);
 			
 			robozin.keyPress(KeyEvent.VK_V);
 			robozin.keyRelease(KeyEvent.VK_V);
 			
-			robozin.keyPress(KeyEvent.VK_A);
-			robozin.keyRelease(KeyEvent.VK_A);
+			robozin.keyPress(KeyEvent.VK_I);
+			robozin.keyRelease(KeyEvent.VK_I);
+			
+			robozin.keyPress(KeyEvent.VK_C);
+			robozin.keyRelease(KeyEvent.VK_C);
+			
+			robozin.keyPress(KeyEvent.VK_T);
+			robozin.keyRelease(KeyEvent.VK_T);
+			
+			robozin.keyPress(KeyEvent.VK_O);
+			robozin.keyRelease(KeyEvent.VK_O);
+			
+			robozin.keyPress(KeyEvent.VK_R);
+			robozin.keyRelease(KeyEvent.VK_R);
 			
 			robozin.keyPress(KeyEvent.VK_ENTER);
 			robozin.keyRelease(KeyEvent.VK_ENTER);
 			//FIM
-			
-			//JOptionPane.showMessageDialog(null, "TROLEEEEEEEEEEI :)", "TROLADO!!!", JOptionPane.WARNING_MESSAGE);
 			
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
