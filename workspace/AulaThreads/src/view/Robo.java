44cb5a0a841c677c949793af70b4af83957a028a
RaphaelMendes203 <raphael.victor204@gmail.com>
2019-03-05 17:07:42 -0300

    Alterações na classe Robo

--- workspace/AulaThreads/src/view/Robo.java
+++ workspace/AulaThreads/src/view/Robo.java
@@ -8,6 +8,7 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
 
 
 public class Robo {
@@ -18,12 +19,14 @@ public class Robo {
 		Dimension d = tk.getScreenSize();
 		
 		try {
-			Robot robozin = new Robot();
+			Robot robo = new Robot();
+			char[] letters;
+			String search = JOptionPane.showInputDialog(null, "Oque você deseja pesquisar no google ?", "SEARCH", JOptionPane.QUESTION_MESSAGE);
 			
 			//robozin.mouseMove(60, d.height - 10);
 			
-			robozin.delay(100);
-			robozin.setAutoDelay(100);
+			robo.delay(100);
+			robo.setAutoDelay(100);
 			
 			//robozin.mousePress(InputEvent.BUTTON1_MASK);
 			//robozin.mouseRelease(InputEvent.BUTTON1_MASK);
@@ -35,115 +38,27 @@ public class Robo {
 				e1.printStackTrace();
 			}
 			
-			robozin.keyPress(KeyEvent.VK_WINDOWS);
-			robozin.keyPress(KeyEvent.VK_R);
-			robozin.keyRelease(KeyEvent.VK_R);
-			robozin.keyRelease(KeyEvent.VK_WINDOWS);
+			robo.keyPress(KeyEvent.VK_WINDOWS);
+			robo.keyPress(KeyEvent.VK_R);
+			robo.keyRelease(KeyEvent.VK_R);
+			robo.keyRelease(KeyEvent.VK_WINDOWS);
 			
-			//ESCREVE CMD
-			robozin.keyPress(KeyEvent.VK_C);
-			robozin.keyRelease(KeyEvent.VK_C);
+			writeWord(robo, "CMD");
 			
-			robozin.keyPress(KeyEvent.VK_M);
-			robozin.keyRelease(KeyEvent.VK_M);
+			robo.keyPress(KeyEvent.VK_ENTER);
+			robo.keyRelease(KeyEvent.VK_ENTER);
 			
-			robozin.keyPress(KeyEvent.VK_D);
-			robozin.keyRelease(KeyEvent.VK_D);
-			//FIM
-			
-			
-			robozin.keyPress(KeyEvent.VK_ENTER);
-			robozin.keyRelease(KeyEvent.VK_ENTER);
-			
-			//ESCREVE START
-			robozin.keyPress(KeyEvent.VK_S);
-			robozin.keyRelease(KeyEvent.VK_S);
-			
-			robozin.keyPress(KeyEvent.VK_T);
-			robozin.keyRelease(KeyEvent.VK_T);
-			
-			robozin.keyPress(KeyEvent.VK_A);
-			robozin.keyRelease(KeyEvent.VK_A);
-			
-			robozin.keyPress(KeyEvent.VK_R);
-			robozin.keyRelease(KeyEvent.VK_R);
-			
-			robozin.keyPress(KeyEvent.VK_T);
-			robozin.keyRelease(KeyEvent.VK_T);
-			//FIM
-			
-			robozin.keyPress(KeyEvent.VK_SPACE);
-			robozin.keyRelease(KeyEvent.VK_SPACE);
-			
-			//ESCREVE FIREFOX
-			robozin.keyPress(KeyEvent.VK_F);
-			robozin.keyRelease(KeyEvent.VK_F);
-			
-			robozin.keyPress(KeyEvent.VK_I);
-			robozin.keyRelease(KeyEvent.VK_I);
-			
-			robozin.keyPress(KeyEvent.VK_R);
-			robozin.keyRelease(KeyEvent.VK_R);
-			
-			robozin.keyPress(KeyEvent.VK_E);
-			robozin.keyRelease(KeyEvent.VK_E);
-			
-			robozin.keyPress(KeyEvent.VK_F);
-			robozin.keyRelease(KeyEvent.VK_F);
-			
-			robozin.keyPress(KeyEvent.VK_O);
-			robozin.keyRelease(KeyEvent.VK_O);
-			
-			robozin.keyPress(KeyEvent.VK_X);
-			robozin.keyRelease(KeyEvent.VK_X);
-			//FIM
-			
-			
-			robozin.keyPress(KeyEvent.VK_SPACE);
-			robozin.keyRelease(KeyEvent.VK_SPACE);
-			
-			
-			//GOOGLE.COM
-			robozin.keyPress(KeyEvent.VK_G);
-			robozin.keyRelease(KeyEvent.VK_G);
-			
-			robozin.keyPress(KeyEvent.VK_O);
-			robozin.keyRelease(KeyEvent.VK_O);
-			
-			robozin.keyPress(KeyEvent.VK_O);
-			robozin.keyRelease(KeyEvent.VK_O);
-			
-			robozin.keyPress(KeyEvent.VK_G);
-			robozin.keyRelease(KeyEvent.VK_G);
-			
-			robozin.keyPress(KeyEvent.VK_L);
-			robozin.keyRelease(KeyEvent.VK_L);
-			
-			robozin.keyPress(KeyEvent.VK_E);
-			robozin.keyRelease(KeyEvent.VK_E);
-			
-			robozin.keyPress(KeyEvent.VK_PERIOD);
-			robozin.keyRelease(KeyEvent.VK_PERIOD);
-			
-			robozin.keyPress(KeyEvent.VK_C);
-			robozin.keyRelease(KeyEvent.VK_C);
-			
-			robozin.keyPress(KeyEvent.VK_O);
-			robozin.keyRelease(KeyEvent.VK_O);
-			
-			robozin.keyPress(KeyEvent.VK_M);
-			robozin.keyRelease(KeyEvent.VK_M);
-			//FIM
+			writeWord(robo, "START FIREFOX WWW.GOOGLE.COM");
 			
 			try {
-				Thread.sleep(2000);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			robozin.keyPress(KeyEvent.VK_ENTER);
-			robozin.keyRelease(KeyEvent.VK_ENTER);
+			robo.keyPress(KeyEvent.VK_ENTER);
+			robo.keyRelease(KeyEvent.VK_ENTER);
 			
 			try {
 				Thread.sleep(5000);
@@ -152,53 +67,10 @@ public class Robo {
 				e.printStackTrace();
 			}
 			
+			writeWord(robo, search.toUpperCase());
 			
-			//ESCREVE JAVA
-			robozin.keyPress(KeyEvent.VK_R);
-			robozin.keyRelease(KeyEvent.VK_R);
-			
-			robozin.keyPress(KeyEvent.VK_A);
-			robozin.keyRelease(KeyEvent.VK_A);
-			
-			robozin.keyPress(KeyEvent.VK_P);
-			robozin.keyRelease(KeyEvent.VK_P);
-			
-			robozin.keyPress(KeyEvent.VK_H);
-			robozin.keyRelease(KeyEvent.VK_H);
-			
-			robozin.keyPress(KeyEvent.VK_A);
-			robozin.keyRelease(KeyEvent.VK_A);
-			
-			robozin.keyPress(KeyEvent.VK_E);
-			robozin.keyRelease(KeyEvent.VK_E);
-			
-			robozin.keyPress(KeyEvent.VK_L);
-			robozin.keyRelease(KeyEvent.VK_L);
-			
-			robozin.keyPress(KeyEvent.VK_SPACE);
-			robozin.keyRelease(KeyEvent.VK_SPACE);
-			
-			robozin.keyPress(KeyEvent.VK_V);
-			robozin.keyRelease(KeyEvent.VK_V);
-			
-			robozin.keyPress(KeyEvent.VK_I);
-			robozin.keyRelease(KeyEvent.VK_I);
-			
-			robozin.keyPress(KeyEvent.VK_C);
-			robozin.keyRelease(KeyEvent.VK_C);
-			
-			robozin.keyPress(KeyEvent.VK_T);
-			robozin.keyRelease(KeyEvent.VK_T);
-			
-			robozin.keyPress(KeyEvent.VK_O);
-			robozin.keyRelease(KeyEvent.VK_O);
-			
-			robozin.keyPress(KeyEvent.VK_R);
-			robozin.keyRelease(KeyEvent.VK_R);
-			
-			robozin.keyPress(KeyEvent.VK_ENTER);
-			robozin.keyRelease(KeyEvent.VK_ENTER);
-			//FIM
+			robo.keyPress(KeyEvent.VK_ENTER);
+			robo.keyRelease(KeyEvent.VK_ENTER);
 			
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
@@ -206,5 +78,20 @@ public class Robo {
 		}
 
 	}
+	
+	private static void writeWord(Robot robo, String word) {
+		int[] letters = retornaLetters(word);
+		for(int i=0; i<letters.length; i++) {
+			robo.keyPress(letters[i]);
+			robo.keyRelease(letters[i]);
+		}
+	}
 
+	private static int[] retornaLetters(String word) {
+		int[] letters = new int[word.length()];
+		for(int i=0; i<word.length(); i++) {
+			letters[i] = (int) word.charAt(i);
+		}
+		return letters;
+	}
 }
